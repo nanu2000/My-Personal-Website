465b339caec6a56f7960954d374883da05008a99
Richie <richiesikra@gmail.com>
2016-12-17 17:09:42 -0800

    added <?php ?> tags

--- Logo.php
+++ Logo.php
@@ -2,13 +2,14 @@
 
 function outputLogo()
 {
-    echo
-    ('  
+?>
+
+
     <div id = "RichieTextWrapper">
         <div id ="RichieText">Richie Sikra</div>  
         <span id = "RichieTextSubtitle">Developer | Designer | Creator</span>
-    </div>  
-    ');
-}
+    </div>
 
+<?php
+}
 ?>
