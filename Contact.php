6e77a8f463afdaf09d8f8f43c9741544887caf14
Richie <richiesikra@gmail.com>
2017-01-09 09:28:30 -0800

    Updated Contact html

--- Contact.php
+++ Contact.php
@@ -14,14 +14,10 @@ echo
 '
 <div class="nonFlexBG"">
 <div class = "textCenter" id ="ContactPageTitle">Contact</div>
-</div>
-<div class="nonFlexBG"">
-<p class = "textCenter ContactPageText">The best way to contact me is through email, but if you message me on Youtube or any other social media outlet I am still likey to answer, just not at a reasonable time.</p>
-</div>
-<div class="nonFlexBG"">
-<p class = "textCenter ContactPageText">Name: Richie Sikra
+<p class = "ContactPageText">The best way to contact me is through email, but if you message me on Youtube or any other social media outlet I am still likey to answer, just not at a reasonable time.</p>
+<p class = "ContactPageText"><b>Name</b>: Richie Sikra
 <br>
-Email: richie@devrichie.com</p>
+<b>Email</b>: richie@devrichie.com</p>
 </div>
 
 
