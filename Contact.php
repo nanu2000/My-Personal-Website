4825ab104c8b29f3948d3a9d4bd4eb5ae8658d4c
Richie <richiesikra@gmail.com>
2017-01-09 08:48:59 -0800

    Updated Contact Page Styling

--- Contact.php
+++ Contact.php
@@ -4,7 +4,7 @@ include('Logo.php');
 include('NavBar.php');
 
 
-startContent(array(), array(), array());
+startContent(array("Styling/ContactPageStyle.css"), array(), array());
 outputLogo();
 startContainerStyle();
 displayNavbar(NAV_OPTIONS::CONTACT, "");
@@ -12,16 +12,17 @@ displayNavbar(NAV_OPTIONS::CONTACT, "");
 echo
 (
 '
-    <div class="nonFlexBG"">
-        <center>
-           <p style = "font-family: '."'Raleway/'".', sans-serif;padding:10px;">The best way to contact me is through email; but if you message me on twitter or youtube I am still likey to answer, just not at a reasonable time.</p>
-                   </center>
-   </div>
-    <div class="nonFlexBG">
-        <center>
-             <p style = "font-family: '."'Raleway/'".', sans-serif;padding:10px;">Email: richiesikra@gmail.com</p>
-        </center>
-   </div>
+<div class="nonFlexBG"">
+<div class = "textCenter" id ="ContactPageTitle">Contact</div>
+</div>
+<div class="nonFlexBG"">
+<p class = "textCenter ContactPageText">The best way to contact me is through email, but if you message me on Youtube or any other social media outlet I am still likey to answer, just not at a reasonable time.</p>
+</div>
+<div class="nonFlexBG"">
+<p class = "textCenter ContactPageText">Name: Richie Sikra
+<br>
+Email: richie@devrichie.com</p>
+</div>
 
 
 ');
