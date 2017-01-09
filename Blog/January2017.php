f094780a89db199f1030fb024c593041ce93dc9f
Richie <richiesikra@gmail.com>
2017-01-09 07:04:30 -0800

    Started blog post - moved .featureList to globalStyling

--- Blog/January2017.php
+++ Blog/January2017.php
@@ -12,13 +12,17 @@ displayNavbar(NAV_OPTIONS::BLOG, "../");
 include ('blogNavbar.php');
 
 
-startBlogPost( "January 8th" , "A New Year?");
+startBlogPost( "January 9th" , "A New Year? + Caught in the Moment");
 
 ?>
 
-test
 
-
+<p>
+A lot has happened, but do I even need to say that? It's been two months since my last blog post. Of course a lot has happened!
+</p>
+<ol>
+    <li>test</li>
+</ol>
 
 
 <?php
--- Blog/blogNavbar.php
+++ Blog/blogNavbar.php
@@ -10,7 +10,8 @@ echo
 <a href="April2016.php" class = "TextLink">April 2016</a> |
 <a href="June2016.php" class = "TextLink">June 2016</a> |
 <a href="September2016.php" class = "TextLink">September 2016</a> |
-<a href="November2016.php" class = "TextLink">November 2016</a>
+<a href="November2016.php" class = "TextLink">November 2016</a> |
+<a href="January2017.php" class = "TextLink">January 2017</a>
 </center>
 </div>
 ');
