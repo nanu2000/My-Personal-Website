fe6120a62a36644d861bbc44303c02b40164fac6
Richie <richiesikra@gmail.com>
2017-01-12 11:04:35 -0800

    Clean JS

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -1,20 +1,22 @@
-/*
- * 
- * Temporary Code, will be revised.
- * 
- */
-
+/*Settings for flex items on hover.*/
 ColorChanger.prototype.HIGHLIGHT_COLOR_PERCENT          = .6;
 ColorChanger.prototype.SHADOW_COLOR_PERCENT             = -.3;
 ColorChanger.prototype.TIME_UNTIL_HOVER_OVER_COLOR      = 1;
 ColorChanger.prototype.TIME_UNTIL_HOVER_OUT_COLOR       = 3;
 
+/*Changes backgroundColorElement's background color based on flexItems data-hovercolor when flexItem is hovered*/
 function ColorChanger(backgroundColorElement)
 {
+    /*Default Background color when no flexItem is hovered. This is set to the backgroundColorElement's initial background color in the start function.*/
     this.defaultColor           = "black";
     this.backgroundColorElement   = backgroundColorElement;
  
-    this._onMouseOverHandler = function(element) 
+    /*******************************************************************************************************
+     * flexItems onMouseOver function
+     * Changes backgroundColorElement's background color to flexItems data-hovercolor. 
+     * Additionally will change backgroundColorElement's transition duration to TIME_UNTIL_HOVER_OVER_COLOR.
+     *******************************************************************************************************/
+    this._onMouseOverHandler = function() 
     {
         var colorChanger = this;
         var backgroundColorElementStyle = this.backgroundColorElement.style;
@@ -22,10 +24,15 @@ function ColorChanger(backgroundColorElement)
         return function() 
         {
             backgroundColorElementStyle.transitionDuration  = colorChanger.TIME_UNTIL_HOVER_OVER_COLOR + "s";  
-            backgroundColorElementStyle.backgroundColor     = element.shadowHoverColor;
+            backgroundColorElementStyle.backgroundColor     = this.shadowHoverColor;
         };
     };
     
+    /*******************************************************************************************************
+     * flexItems onMouseOut function
+     * Changes backgroundColorElement's background color to default color (initial)
+     * Additionally will change backgroundColorElement's transition duration to TIME_UNTIL_HOVER_OUT_COLOR.
+     *******************************************************************************************************/
     this._onMouseOutHandler = function() 
     {
         var colorChanger = this;
@@ -38,6 +45,11 @@ function ColorChanger(backgroundColorElement)
         };
     };
     
+    
+    /*******************************************************************************************************
+     * Takes a single flexItem, reads it's data-hovercolor and highlights + shadows it's textBg borders + background.
+     * Also set's onMouseOver and onMouseOut functions
+    *******************************************************************************************************/
     this._processFlexItem = function(currentFlexItem)
     {
         
@@ -53,11 +65,14 @@ function ColorChanger(backgroundColorElement)
         textWrapperStyle.borderTopColor    = currentFlexItem.highlightHoverColor;
         textWrapperStyle.borderBottomColor = currentFlexItem.shadowHoverColor;
 
-        currentFlexItem.onmouseover    = this._onMouseOverHandler(currentFlexItem);
+        currentFlexItem.onmouseover    = this._onMouseOverHandler();
         currentFlexItem.onmouseout     = this._onMouseOutHandler();
         
     };
     
+    /*******************************************************************************************************
+     * Initialises everything. (processes flexItems and retrieves initial background color)
+    *******************************************************************************************************/
     this.start = function()
     {
         this.defaultColor = this.backgroundColorElement.style.backgroundColor;
@@ -86,6 +101,10 @@ function colorLuminance(hex, lum)
     return rgb;
 }
 
+ /*******************************************************************************************************
+ * Runs javascript needed for mobile. (hovering and such.)
+ * Appends mobile class to text_links and appends hover class to flexItems 
+ *******************************************************************************************************/
 function executeMobileJavascript()
 {
     var elements    = document.getElementsByClassName("flexItem");
@@ -101,38 +120,61 @@ function executeMobileJavascript()
     }
 }
 
+/*toggles more button to be selected. white with black text for button and children are set to block display.*/
+function moreButtonOn(showNavbarAStyle, navbarMoreStyle)
+{
+    showNavbarAStyle.color              = "black";
+    showNavbarAStyle.backgroundColor    = "white";
+    navbarMoreStyle.display             = "block";
+}
+
+/*toggles more button to be not selected. (initial coloring and children hidden)*/
+function moreButtonOff(showNavbarAStyle, navbarMoreStyle)
+{
+    showNavbarAStyle.color              = "";
+    showNavbarAStyle.backgroundColor    = "";
+    navbarMoreStyle.display             = "none";
+}
+
+
+ /*******************************************************************************************************
+ *Javascipt to be ran when the more button is pressed.
+ *Toggles display of more Items and adds selection coloring 
+ *******************************************************************************************************/
 moreMenuOnclickHandler = function(showNavbarAStyle, navbarMoreStyle) 
 {
     return function() 
     {
         if(navbarMoreStyle.display !== "none")
         {
-            showNavbarAStyle.color              = "";
-            showNavbarAStyle.backgroundColor    = "";
-            navbarMoreStyle.display             = "none";
+            moreButtonOff(showNavbarAStyle, navbarMoreStyle);
         }
         else
         {
-            showNavbarAStyle.color              = "black";
-            showNavbarAStyle.backgroundColor    = "white";
-            navbarMoreStyle.display             = "block";
+            moreButtonOn(showNavbarAStyle, navbarMoreStyle);
         }
     };
 };
-    
+   
+ /*******************************************************************************************************
+ * Sets onclick handler for 'more' button, and toggles the more button to be off.
+ *******************************************************************************************************/
 function setupMoreMenu()
-{
-    document.getElementById("navbar_more").style.display = "none";
+{    
+    var showNavbarAStyle = document.getElementById("show_navbar").querySelector('a').style;
+    var navbarMoreStyle = document.getElementById("navbar_more").style;
     
-    document.getElementById("show_navbar").onclick = moreMenuOnclickHandler
-    (
-        document.getElementById("show_navbar").querySelector('a').style,
-        document.getElementById("navbar_more").style
-    );
+    moreButtonOff(showNavbarAStyle, navbarMoreStyle);
+    
+    document.getElementById("show_navbar").onclick = moreMenuOnclickHandler(showNavbarAStyle, navbarMoreStyle);
 };
 
-window.onload = function() 
-{        
+/**************************
+ *Runs when file is loaded.
+ *This is the entry function.
+ **************************/
+function runJavascript()
+{
     if ("transitionProperty" in document.body.style)
     {
         var bGColorChange = new ColorChanger(document.body);
@@ -144,5 +186,8 @@ window.onload = function()
         executeMobileJavascript();
     }
     
-    setupMoreMenu();
-};
+    setupMoreMenu();    
+}
+
+/*self explanatory*/
+runJavascript();
