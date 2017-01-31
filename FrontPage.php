2c8c4bac179253ec8ee73d091660922355ae7099
Richie <richiesikra@gmail.com>
2017-01-30 17:49:06 -0800

    Changed Padding and Font sizes

--- FrontPage.php
+++ FrontPage.php
@@ -25,10 +25,6 @@ startContentContainer();
     Lorem ipsum dolor sit amet, <b>consectetur</b> adipiscing elit. Aliquam lorem orci, maximus sed arcu et, ornare feugiat nisi. Nulla aliquam tortor a porttitor condimentum. Integer scelerisque nunc semper urna feugiat mattis. Fusce id lobortis libero, sollicitudin imperdiet sem. Cum sociis natoque penatibus et <b>magnis dis parturient montes</b>, nascetur ridiculus mus. Nunc libero justo, ultrices et nibh a, placerat ultrices ante. Proin elementum feugiat placerat. Nam sit amet nisl purus. Aliquam sed nisl id metus ultricies gravida. Morbi augue nibh, placerat eu eros eget, lobortis gravida felis. Integer faucibus dui non ex ultrices, in <b>consectetur leo varius.</b> Proin sit amet tellus et elit commodo condimentum. Fusce ac est ornare, semper nisl eget, lacinia sapien. Proin sed aliquam lectus, sed imperdiet ligula. Integer dictum aliquet metus.
 </p>
 
-<p>
-    Lorem ipsum dolor sit amet, <b>consectetur</b> adipiscing elit. Aliquam lorem orci, maximus sed arcu et, ornare feugiat nisi. Nulla aliquam tortor a porttitor condimentum. Integer scelerisque nunc semper urna feugiat mattis. Fusce id lobortis libero, sollicitudin imperdiet sem. Cum sociis natoque penatibus et <b>magnis dis parturient montes</b>, nascetur ridiculus mus. Nunc libero justo, ultrices et nibh a, placerat ultrices ante. Proin elementum feugiat placerat. Nam sit amet nisl purus. Aliquam sed nisl id metus ultricies gravida. Morbi augue nibh, placerat eu eros eget, lobortis gravida felis. Integer faucibus dui non ex ultrices, in <b>consectetur leo varius.</b> Proin sit amet tellus et elit commodo condimentum. Fusce ac est ornare, semper nisl eget, lacinia sapien. Proin sed aliquam lectus, sed imperdiet ligula. Integer dictum aliquet metus.
-</p>
-
 </div>
 
 
